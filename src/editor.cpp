848af1bc0df028ac619b4e34b86227e52ba58b79
inixyz <alexandru.ene2702@gmail.com>
2023-09-16 00:51:37 +0300

    window focus for editor

--- src/editor.cpp
+++ src/editor.cpp
@@ -2,9 +2,11 @@
 #include "render.hpp"
 
 void Editor::update(Window& window, Map& map){
-	sf::Vector2i mouse_position = sf::Mouse::getPosition(window.render_window);
-	cursor_cell.position = (sf::Vector2u)window.render_window.mapPixelToCoords(mouse_position);
-	cursor_cell.position /= Render::get_instance().CELL_SIZE;
+	if(window.render_window.hasFocus()){
+		sf::Vector2i mouse_position = sf::Mouse::getPosition(window.render_window);
+		cursor_cell.position = (sf::Vector2u)window.render_window.mapPixelToCoords(mouse_position);
+		cursor_cell.position /= Render::get_instance().CELL_SIZE;
+	}
 
 	cursor_cell.valid = cursor_cell.position.x < map.width && cursor_cell.position.y < map.height;
 }
